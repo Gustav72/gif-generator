034552fad9e6d88b9f6c031564e7878a1eb00e7d
Gustav72 <wolfgangkirstein7@gmail.com>
2023-01-23 08:15:59 -0500

    add search button

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const img = document.querySelector('img');
+const button = document.querySelector('button')
 
 let searchTerm = 'puppy'
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=nlthNgtAgJQWr97ibEs9WmgelEs3U2ve&s=' + searchTerm, {
-    mode: 'cors'
+
+button.addEventListener('click', function() {
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=nlthNgtAgJQWr97ibEs9WmgelEs3U2ve&s=' + searchTerm, {
+        mode: 'cors'
+    })
+    
+    .then(function(response) {
+        return response.json();
+    })
+    
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+    });
 })
 
-.then(function(response) {
-    return response.json();
-})
 
-.then(function(response) {
-    img.src = response.data.images.original.url;
-});
