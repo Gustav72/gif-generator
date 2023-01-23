8c02805bea0e2b18e782dd0ca6aeff3fc27541b8
Gustav72 <wolfgangkirstein7@gmail.com>
2023-01-22 22:18:02 -0500

    test Giphy API

---
+++ script.js
@@ -0,0 +1,13 @@
+const img = document.querySelector('img');
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=nlthNgtAgJQWr97ibEs9WmgelEs3U2ve&s=cats', {
+    mode: 'cors'
+})
+
+.then(function(response) {
+    return response.json();
+})
+
+.then(function(response) {
+    img.src = response.data.images.original.url;
+});
